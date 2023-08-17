0ccddc4802b491652214c1215440270033e92ff9
Gyozadaigaku <gaspardcube15@gmail.com>
2023-08-17 15:58:50 +0900

    ✨ Static rendering in a server component

---
+++ src/libs/microcms.ts
@@ -0,0 +1,56 @@
+import { createClient } from "microcms-js-sdk";
+import type {
+  MicroCMSQueries,
+  MicroCMSImage,
+  MicroCMSDate,
+} from "microcms-js-sdk";
+
+export type Blog = {
+  id: string;
+  title: string;
+  content: string;
+  eyecatch?: MicroCMSImage;
+} & MicroCMSDate;
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("MICROCMS_API_KEY is required");
+}
+
+export const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+// Get a list of blog posts
+export const getList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Blog>({
+    endpoint: "blogs",
+    queries,
+  });
+
+  // Add a delay effect to make data retrieval visually noticeable
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  return listData;
+};
+
+// Get the details of a specific blog post
+export const getDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  const detailData = await client.getListDetail<Blog>({
+    endpoint: "blogs",
+    contentId,
+    queries,
+  });
+
+  // Add a delay effect to make data retrieval visually noticeable
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  return detailData;
+};
