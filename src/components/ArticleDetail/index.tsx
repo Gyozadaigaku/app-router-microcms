caa573eb36f742e49efcc101c9930a854e078ac0
Gyozadaigaku <gaspardcube15@gmail.com>
2023-08-20 03:50:01 +0900

    🐛 Render HTML directly

--- src/components/ArticleDetail/index.tsx
+++ src/components/ArticleDetail/index.tsx
@@ -2,16 +2,18 @@ import { Article } from "@/libs/microcms";
 import parse from "html-react-parser";
 import styles from "./index.module.css";
 
-interface ArticleDetailProps {
+type ArticleDetailProps = {
   article: Article;
-}
+};
 
 export const ArticleDetail = async ({ article }: ArticleDetailProps) => {
-  // Get the time the page was generated.
   return (
     <>
-      <h1 className={styles.title}>{article.title}</h1>
       {/* TODO: Use `html-react-parser` instead of `dangerouslySetInnerHTML` in the future */}
+      <h1
+        className={styles.title}
+        dangerouslySetInnerHTML={{ __html: article.title }}
+      />
       {article.content.map((item, index) => (
         <div key={index}>
           {item.richEditor && (
